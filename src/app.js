fbc54ef4b051d97c80b4d21d9eca2cd055d6dd94
JR10 <hamdyjs@gmail.com>
2015-09-17 02:20:16 +0200

    [webserver] Re-order listening after event handling

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ var port = 3000; // TEMP
 var app = express();
 require('http')
   .Server(app)
-  .listen(port)
   .on('listening', function() {
     debug('Server started listening on '+port);
   })
   .on('error', function(error) {
     throw error;
-  });
+  })
+  .listen(port);
