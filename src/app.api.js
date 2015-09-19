a206202e9cba650ae7e3d14a145352e0eba35029
JR10 <hamdyjs@gmail.com>
2015-09-19 06:04:42 +0200

    [webserver] Route /api/version to output project version
    The version is retrieved from the package.json file

--- src/app.api.js
+++ src/app.api.js
@@ -1,8 +1,14 @@
 var router = require('express').Router(),
     path   = require('path');
 
+var pkg = require('./../package');
+
 router.get('/config', function(req, res) {
   res.sendFile(path.join(__dirname, '..', 'config.json'));
+});
+
+router.get('/version', function(req, res) {
+  res.end(pkg.version);
 });
 
 module.exports = router;
