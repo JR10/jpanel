60e0056e968c4ea88e5471eeaa964f69a8ad102f
JR10 <hamdyjs@gmail.com>
2015-09-19 06:08:45 +0200

    [ng] Add jVersion directive

---
+++ public/ng/directives/version.dir.js
@@ -0,0 +1,17 @@
+angular
+  .module('app')
+  .directive('jVersion', function() {
+    return {
+      restrict: 'E',
+      template: '<span ng-bind="vm.version"></span>',
+      controller: ['$http', function($http) {
+        var self = this;
+        $http
+          .get('/api/version')
+          .then(function(response) {
+            self.version = response.data;
+          });
+      }],
+      controllerAs: 'vm'
+    };
+  });
